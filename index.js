88dd09aae4238e22614cc12f6f90710bfa2af0b6
Achhaya Pathak <achhayapathak11@gmail.com>
2023-03-10 00:01:07 +0530

    added gitignore

--- index.js
+++ index.js
@@ -21,25 +21,6 @@ app.use(cors({
   origin : ["http://localhost:3000", "https://shailkas-blog.onrender.com"],
 }));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
-
-// const corsOptions ={
-//   origin:'http://localhost:3000', 
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-// app.use(cors());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
